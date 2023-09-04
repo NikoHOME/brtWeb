28c72de402c029e8a10f852081fd6280d3c6dceb
NikoHOME <wiktor.gawronski@gmail.com>
2023-09-04 13:54:57 +0200

    Fix form and fonts

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const text_wall = document.querySelector(".text_wall")
 if(video)
 {
     video.addEventListener("ended", (event) => {
-        // sessionStorage.setItem("visited_before","true")
+        sessionStorage.setItem("visited_before","true")
         video.style.opacity = '0.3'
         content.style.opacity = "1"
         document.documentElement.style.overflow = "auto"
