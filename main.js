6336ff4580e8ea1314f3c72e26a0f51017dce89c
NikoHOME <wiktor.gawronski@gmail.com>
2023-09-04 14:03:39 +0200

    Fix title screen buttons

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ const text_wall = document.querySelector(".text_wall")
 
 if(video)
 {
+    
     video.addEventListener("ended", (event) => {
-        sessionStorage.setItem("visited_before","true")
+        let start_menu = document.querySelector(".start_menu");
+        //sessionStorage.setItem("visited_before","true")
         video.style.opacity = '0.3'
         content.style.opacity = "1"
         document.documentElement.style.overflow = "auto"
+        start_menu.style.display = "none"
         
     }, {once: true});
 }
